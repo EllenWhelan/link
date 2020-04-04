630ac3ddb058b2c2bea5da00af9ed2d9f44cf043
zuo132 <47151679+zuo132@users.noreply.github.com>
2020-04-04 21:45:09 +0100

    add popup styling

--- src/App.js
+++ src/App.js
@@ -2,205 +2,200 @@ import React, { Component } from "react";
 import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      location: window.location.href.substr(23),
-      filters: [],
-      pub_filters: "",
-      pub_category: "",
-      queryString: "",
-      url: window.location.href
-    };
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  async handleChange(event) {
-    const { name, value, type} = event.target;
-    if (type === "checkbox") {
-      if (this.state.filters.includes(value)) {
-        this.state.filters = this.state.filters.filter(f => f !== value);
-      } else {
-        this.state.filters.push(value);
-      }
-      this.setState(
-        {
-          pub_filters: this.state.filters.join("%2C")
-        },
-        () => this.updateQueryString()
-      );
-    } else {
-      this.setState({ [name]: value }, () => this.updateQueryString());
+    constructor() {
+        super();
+        this.state = {
+            location: window.location.href.substr(23),
+            filters: [],
+            pub_filters: "",
+            pub_category: "",
+            queryString: "",
+            url: window.location.href,
+        };
+        this.handleChange = this.handleChange.bind(this);
     }
-  }
 
-  updateQueryString() {
-    let queryString = "";
-    if (this.state.pub_category.length !== 0)
-      queryString += `pub_category=${this.state.pub_category}&`;
-    if (this.state.pub_filters.length !== 0)
-      queryString += `pub_filters=${this.state.pub_filters}`;
-    this.setState({
-      queryString: queryString
-    });
-  }
+    async handleChange(event) {
+        const { name, value, type } = event.target;
+        if (type === "checkbox") {
+            if (this.state.filters.includes(value)) {
+                this.state.filters = this.state.filters.filter(
+                    (f) => f !== value
+                );
+            } else {
+                this.state.filters.push(value);
+            }
+            this.setState(
+                {
+                    pub_filters: this.state.filters.join("%2C"),
+                },
+                () => this.updateQueryString()
+            );
+        } else {
+            this.setState({ [name]: value }, () => this.updateQueryString());
+        }
+    }
 
-  render() {
-    return (
-    <div>
-      <form>
+    updateQueryString() {
+        let queryString = "";
+        if (this.state.pub_category.length !== 0)
+            queryString += `pub_category=${this.state.pub_category}&`;
+        if (this.state.pub_filters.length !== 0)
+            queryString += `pub_filters=${this.state.pub_filters}`;
+        this.setState({
+            queryString: queryString,
+        });
+    }
 
-        <h1>
-        <img className = "logo"  alt = "logo" src={"logo192.png"}  width = "220" height = "80"/> 
-        
-        </h1>
-        <text className = "title">Book a Minder</text>
-      
-        <h2>
-        <text className= "filterHead">Filters</text>
-        </h2>
-      <div id= "filters">
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="new_borns"
-            onChange={this.handleChange}
-          />{" "}
-          New borns
-        </label>
+    render() {
+        return (
+            <div className='popup-container'>
+                <img
+                    className='logo'
+                    alt='logo'
+                    src={"logo192.png"}
+                    width='220'
+                    height='80'
+                />
+                <h1 className='popup-title'>Book a Minder</h1>
+                <h2 className='popup-subtitle'>Filters: </h2>
+                <div className='oogo-form'>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='nb'
+                            name='pub_filters'
+                            value='new_borns'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='nb'>New Borns</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="non_smoker"
-            onChange={this.handleChange}
-          />{" "}
-          Non smoker
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='ns'
+                            name='pub_filters'
+                            value='non_smoker'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='ns'>None Smoker</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="first_aid"
-            onChange={this.handleChange}
-          />{" "}
-          First aid
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='fa'
+                            name='pub_filters'
+                            value='first_aid'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='fa'>First Aid</label>
+                    </div>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='ot'
+                            name='pub_filters'
+                            value='own_transport'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='ot'>Own Transport</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="own_transport"
-            onChange={this.handleChange}
-          />{" "}
-          Own Transport
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='q'
+                            name='pub_filters'
+                            value='qualifications'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='q'>Qualifications</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="qualifications"
-            onChange={this.handleChange}
-          />{" "}
-          Qualifications
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='ov'
+                            name='pub_filters'
+                            value='overnights'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='ov'>Overnights</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="overnights"
-            onChange={this.handleChange}
-          />{" "}
-          Overnights
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='ev'
+                            name='pub_filters'
+                            value='evenings'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='ev'>Evenings</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="evenings"
-            onChange={this.handleChange}
-          />{" "}
-          Evenings
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='mr'
+                            name='pub_filters'
+                            value='mornings'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='mr'>Mornings</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="mornings"
-            onChange={this.handleChange}
-          />{" "}
-          Mornings
-        </label>
+                    <div>
+                        <input
+                            type='checkbox'
+                            id='ad'
+                            name='pub_filters'
+                            value='all_day'
+                            onChange={this.handleChange}
+                        />
+                        <label htmlFor='ad'>All Day</label>
+                    </div>
 
-        <br />
-        <label>
-          <input
-            type="checkbox"
-            name="pub_filters"
-            value="all_day"
-            onChange={this.handleChange}
-          />{" "}
-          All day
-        </label>
-       </div>
+                    <h2 className='popup-subtitle'>Category: </h2>
+                    <div className='select-container'>
+                        <select
+                            name='pub_category'
+                            value={this.state.pub_category}
+                            onChange={this.handleChange}
+                        >
+                            <option value=''>None</option>
+                            <option value='babysitter'>Babysitter</option>
+                            <option value='babysitter_overnight'>
+                                Overnight Babysitter
+                            </option>
+                            <option value='nanny'>Nanny</option>
+                            <option value='childminder'>Childminder</option>
+                            <option value='day_care'>Daycare</option>
+                            <option value='maternity_nurse'>
+                                Maternity Nurse
+                            </option>
+                        </select>
+                    </div>
 
-        <br />
-        <br />
-        
-        <label >Category:  </label>
-        
-        <select
-          value={this.state.pub_category}
-          defaultValue={{ label: "Select Dept", value: 0 }}
-          onChange={this.handleChange}
-          name="pub_category"
-        >
-          <option value="">none</option>
-          <option value="babysitter"> babysitter</option>
-          <option value="babysitter_overnight">babysitter_overnight</option>
-          <option value="nanny">nanny</option>
-          <option value="childminder">childminder</option>
-          <option value="day_care">day_care</option>
-          <option value="maternity_nurse">maternity_nurse</option>
-        </select>
-
-        <br />
-        <br />
-        
-        <div id="container">
-        <button class="button">Cancel</button>
-        <a
-          className = "submit"
-          href={`https://oogo.herokuapp.com/s?${this.state.queryString}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Submit
-        </a>
-        </div>
-      </form>
-      </div>
-
-      
-      
-      
-    );
-  }
+                    <div className='button-container'>
+                        <button className='submit-button' >
+                            Cancel
+                        </button>
+                        <a
+                            className='submit-button'
+                            href={`https://oogo.herokuapp.com/s?${this.state.queryString}`}
+                            target='_blank'
+                            rel='noopener noreferrer'
+                        >
+                            Submit
+                        </a>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
