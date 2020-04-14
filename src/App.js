9679f43701bb8a32862ecd4a0a6ea4ace5172289
EllenWhelan <whelane5@tcd.ie>
2020-04-14 12:21:54 +0100

    Prepping for integration with Leona

--- src/App.js
+++ src/App.js
@@ -1,231 +1,38 @@
-import React, { Component } from "react";
+import React from "react";
+import emailjs from 'emailjs-com';
 import "./App.css";
-
-class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            location: window.location.href.substr(23),
-            filters: [],
-            pub_filters: "",
-            pub_category: "",
-            queryString: "",
-            url: window.location.href,
-        };
-        this.handleChange = this.handleChange.bind(this);
-		//this.handleClick = this.handleClick.bind(this);
-    }
-
-	  /*async handleClick(e){
-
-		const {
-			 REACT_APP_EMAILJS_RECEIVER: receiverEmail,
-			 REACT_APP_EMAILJS_TEMPLATEID: template,
-			 REACT_APP_EMAILJS_USERID: user
-		   } = this.props.env
-
-		   Form.mailTime(
-			 template,
-			 this.props.senderEmail,
-			 receiverEmail,
-			 this.state.feedback,
-			 user
-		   );
-     }*/
-    async handleChange(event) {
-        const { name, value, type } = event.target;
-        if (type === "checkbox") {
-            if (this.state.filters.includes(value)) {
-                this.state.filters = this.state.filters.filter(
-                    (f) => f !== value
-                );
-            } else {
-                this.state.filters.push(value);
-            }
-            this.setState(
-                {
-                    pub_filters: this.state.filters.join("%2C"),
-                },
-                () => this.updateQueryString()
-            );
-        } else {
-            this.setState({ [name]: value }, () => this.updateQueryString());
-        }
-    }
-
-    updateQueryString() {
-        let queryString = "";
-        if (this.state.pub_category.length !== 0)
-            queryString += `pub_category=${this.state.pub_category}&`;
-        if (this.state.pub_filters.length !== 0)
-            queryString += `pub_filters=${this.state.pub_filters}`;
-        this.setState({
-            queryString: queryString,
+export default function App() {
+    function sendEmail(e){
+        e.preventDefault();
+        console.log(process.env.REACT_APP_SERVICE_ID);
+        console.log(process.env.REACT_APP_TEMPLATE_ID);
+        console.log(process.env.REACT_APP_USER_ID);
+        emailjs.sendForm(process.env.REACT_APP_SERVICE_ID,process.env.REACT_APP_TEMPLATE_ID,e.target,process.env.REACT_APP_USER_ID)
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+    
+            console.log(error.text);
         });
+      }
+      return (
+        <form id="my-form" className="contact-form" onSubmit={sendEmail}>
+          <input type="hidden" name="contact_number" />
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+          <script type="text/javascript"
+            src="https://cdn.jsdelivr.net/npm/emailjs-com@2.4.1/dist/email.min.js">
+            </script>
+            <script type="text/javascript">
+              (function(){
+                  emailjs.init(process.env.REACT_APP_USER_ID)
+              })();
+            </script>
+        </form>
+      );
     }
-
-    render() {
-        return (
-            <div className='popup-container'>
-                <img
-                    className='logo'
-                    alt='logo'
-                    src={"logo192.png"}
-                    width='220'
-                    height='80'
-                />
-                <h1 className='popup-title'>Book a Minder</h1>
-                <h2 className='popup-subtitle'>Filters: </h2>
-                <div className='oogo-form'>
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='nb'
-                            name='pub_filters'
-                            value='new_borns'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='nb'>New Borns</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='ns'
-                            name='pub_filters'
-                            value='non_smoker'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='ns'>None Smoker</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='fa'
-                            name='pub_filters'
-                            value='first_aid'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='fa'>First Aid</label>
-                    </div>
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='ot'
-                            name='pub_filters'
-                            value='own_transport'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='ot'>Own Transport</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='q'
-                            name='pub_filters'
-                            value='qualifications'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='q'>Qualifications</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='ov'
-                            name='pub_filters'
-                            value='overnights'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='ov'>Overnights</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='ev'
-                            name='pub_filters'
-                            value='evenings'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='ev'>Evenings</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='mr'
-                            name='pub_filters'
-                            value='mornings'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='mr'>Mornings</label>
-                    </div>
-
-                    <div>
-                        <input
-                            type='checkbox'
-                            id='ad'
-                            name='pub_filters'
-                            value='all_day'
-                            onChange={this.handleChange}
-                        />
-                        <label htmlFor='ad'>All Day</label>
-                    </div>
-
-                    <h2 className='popup-subtitle'>Category: </h2>
-                    <div className='select-container'>
-                        <select
-                            name='pub_category'
-                            value={this.state.pub_category}
-                            onChange={this.handleChange}
-                        >
-                            <option value=''>None</option>
-                            <option value='babysitter'>Babysitter</option>
-                            <option value='babysitter_overnight'>
-                                Overnight Babysitter
-                            </option>
-                            <option value='nanny'>Nanny</option>
-                            <option value='childminder'>Childminder</option>
-                            <option value='day_care'>Daycare</option>
-                            <option value='maternity_nurse'>
-                                Maternity Nurse
-                            </option>
-                        </select>
-                    </div>
-					<br />
-					<br />
-					
-					<label>
-					Please enter your booking number:
-					</label>
-					<br />
-					  <input
-						type="text"
-						value={this.state.value}
-						onChange={this.handleChange}
-					/>
-
-                    <div className='button-container'>
-                        <button className='submit-button' >
-                            Cancel
-                        </button>
-                        <a
-                            className='submit-button'
-                            href={`https://oogo.herokuapp.com/s?${this.state.queryString}`}
-                            target='_blank'
-                            rel='noopener noreferrer'
-							onClick = {this.handleClick}
-                        >
-                            Submit
-                        </a>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
-
-export default App;
